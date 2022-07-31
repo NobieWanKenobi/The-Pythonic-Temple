6b946d4e313cd098d84cf2af220d17b3f123bf6b
Harry Potter ;D <109922250+NobieWanKenobi@users.noreply.github.com>
2022-07-31 14:48:12 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,3 +1,11 @@
-def hail_friend():
-  print("Hail, friend!")
- hail_friend()
+def hail_friend(x):
+    t = 'Hail ,'
+    print(t, x, '!')
+
+
+hail_friend('Jonathan Joestar')
+
+
+def add_numbers(s, z):
+    y = s + z
+    return y
