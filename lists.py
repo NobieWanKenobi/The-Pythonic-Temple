78cb66ca3477147bd0199a3ace99b5974effe910
Harry Potter ;D <109922250+NobieWanKenobi@users.noreply.github.com>
2022-07-31 11:10:37 -0500

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,7 @@
+order_of_succession= ['Isabelle', 'Grace', 'Charlotte', 'LeBron', 'Steve']
+
+print(f"The first person to exceed is {order_of_succession[0]}")
+print(f"The second person to exceed is {order_of_succession[1]}")
+print(f"The third person to exceed is {order_of_succession[2]}")
+print(f"The foruth person to exceed is {order_of_succession[3]}")
+print(f"The fith person to exceed is {order_of_succession[4]}")
---
+++ variables.py
@@ -0,0 +1,5 @@
+favorite_robot = 'Cedric'
+meaning_of_life = 42
+
+print(f"My favorite robot is {favorite_robot}")
+print(f"What is the meaning of life, the universe, and everything? {meaning_of_life}")
